a459014d575b92c20823fd727ae7f9c9dcf117b8
Jan Jędrzejak <43744269+janjedrzejak@users.noreply.github.com>
2018-11-08 14:14:02 +0100

    Database class

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
-	include('class/classRefueling.php');
+	require('class/classRefueling.inc');
+	require('class/classCar.inc');
+	require('class/classDB.inc');
 
 	$Refueling = new Refueling(100.4, 10, 14,"Diesel");
-	
-	echo $Refueling->getQuantity();
-	echo $Refueling->getMileage();
-	echo $Refueling->getTypeOfFuel();
+	$Car = new Car("FordFocus", "Diesel", 154555);
+
+	$DB = new DB();
+	$DB->connect();
+
+
 ?>
